76cb8937b6b248ba062cffaaa317cce30e18977e
gideon odiase <gideon.odiase@konga.com>
2020-06-13 14:23:49 +0100

    Added boilerplate for Schemas, Queries and Mutations and also gitlab-ci

---
+++ src/schema/mutations/dummy_mutation.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    dummy_function: function () {
+        return {
+            dummy_response: true
+        }
+    },
+}
---
+++ src/schema/schema.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const mutationFields = require('./')
+const queryFields =  require('./query_fields/query_fields')
+const { GraphQLObjectType, GraphQLSchema, } = require('graphql')
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: queryFields
+})
+
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: mutationFields
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery,
+    mutation
+})
