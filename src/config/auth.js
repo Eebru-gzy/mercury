40a2e5f7474dc4b4c9d4344570d0f5b97c827661
unknown <alaoopeyemi101@gmail.com>
2020-06-21 11:15:21 +0100

    refactor comments in auth

--- src/config/auth.js
+++ src/config/auth.js
@@ -2,44 +2,44 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-
-/*
-*handles password encryption and authentication
-* @params {String} bcryptHasher
-* @params {String} jwtCreate
-*/
-
-
 class authentication {
-  async bcryptHahser(password) {
+	/*
+	 *handles password encryption
+	 * @params {String} bcryptHasher
+	 */
+	async bcryptHahser(password) {
 		const salt = await bcrypt.genSalt(process.env.BCRYPTSALT);
-    const bcryptPassword = await bcrypt.hash(password, salt);
-    return bcryptPassword;
-  }
-  
-  async bcryptCompare(bcryptHash, userPass) {
-    const validPassword = await bcrypt.compare(bcryptHash, userPass);
-
-    if (!validPassword) {
-      return res.status(401).json({ message: 'Incorrect Password' });
-    }
-  };
-
-  jwtCreate(userPayload) {
-    const payload = {
-      user: userPayload,
-    };
-    return jwt.sign(payload, process.env.JWTSECRET, { expiresIn: 60 * 60 });
-  }
-
-  jwtVerify(req, res) {
-    try {
+		const bcryptPassword = await bcrypt.hash(password, salt);
+		return bcryptPassword;
+	}
+	/*
+	 * handles password compare with hash
+	 * @params {String, String} bcryptCompare
+	 */
+	async bcryptCompare(bcryptHash, userPass) {
+		const validPassword = await bcrypt.compare(bcryptHash, userPass);
+		if (!validPassword) {
+			return res.status(401).json({ message: "Incorrect Password" });
+		}
+	}
+	/*
+	 * handles token creation
+	 * @params {String} jwtCreate
+	 */
+	jwtCreate(userPayload) {
+		const payload = {
+			user: userPayload,
+		};
+		return jwt.sign(payload, process.env.JWTSECRET, { expiresIn: 60 * 60 });
+	}
+	/*
+   * handles token verification
+	 * @params {Object, Object} jwtVerify
+	 */
+	jwtVerify(req, res) {
+		try {
 			//the token
-      const jwtToken = req.headers.authorization;
-
-      //or this. I dont know  the difference yet.
-      // const jwtToken = req.header('bearer');
-      
+			const jwtToken = req.headers.authorization;
 			if (!jwtToken) {
 				return res.status(403).json({ msg: "Authorization Denied" });
 			}
@@ -48,7 +48,7 @@ class authentication {
 		} catch (err) {
 			res.status(401).json({ message: "Token is not valid" });
 		}
-  }
+	}
 }
 
 module.exports = authentication;
